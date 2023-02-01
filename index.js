f9c2058a3be6c5c7fd7fc591797f2b098c5ea640
Emmanuel Morales <moralesa.e19@gmail.com>
2023-02-01 15:20:35 -0600

    Update

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 let y = window.screen.height;
 let NavVisibility = 0;
+let mainAbout = document.getElementById("main-About")
+let controls = document.getElementById("controls")
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
   let Navbar = document.getElementById('Navbar-1');
   let WsY = window.scrollY
 
-  if(WsY>(NavVisibility+50) || WsY===0){
-    Navbar.style.display="none";
+  if (WsY > (NavVisibility + 50) || WsY === 0) {
+    Navbar.style.display = "none";
     NavVisibility = WsY
-  }else if(WsY<NavVisibility){
-    Navbar.style.display="block"
+  } else if (WsY < NavVisibility) {
+    Navbar.style.display = "block"
     NavVisibility = WsY
   }
-  // const O1 =0;
-  // const O2 =1;
-  // if(WsY<y*(3/4) && Once!==O1){
-  //     console.log(0)
-  //     Navbar.style.opacity=0;
 
-  //     Once = O1;
-  // }else if(WsY>y*(3/4) && Once!==O2){
-  //     console.log(1)
-  //     Navbar.style.opacity=1;
-  //     Once = O2;
-  // }
-  // console.log(y)
-  //console.log(window.scrollY)
-  //console.log(pos)
+  if (WsY > y * 0.80) {
+    mainAbout.style.opacity = 0;
+    controls.style.display = "flex";
+  }
+  else {
+    mainAbout.style.opacity = 1;
+    controls.style.display = "none";
+  }
 })
+
+const Sections = ["#", "Proyectos", "Tecnologias"]
+let counterScroll = 0
+
+document.getElementById("control-up").addEventListener("click", function () {
+  if (counterScroll <= 5) {
+    counterScroll += 1;
+    scrollSection("Proyectos")
+  }
+
+})
+
+document.getElementById("control-down").addEventListener("click", function () {
+  if (counterScroll > 0) {
+    counterScroll -= 1;
+    scrollSection(Sections[counterScroll])
+  }
+
+})
+
+function scrollSection(Section) {
+  const element = document.getElementById(Section);
+  element.scrollIntoView(true);
+}
